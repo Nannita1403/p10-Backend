38102858fc653a36e507432f56bea27e3a3e1782
Nannita1403 <nannimagi@gmail.com>
2024-09-25 14:28:47 +0200

    ok

--- src/api/controllers/events.js
+++ src/api/controllers/events.js
@@ -102,12 +102,11 @@ const updateEvent = async (req,res,next) => {
             newEvent.organizer = oldEvent.organizer;
            }
     
-          const eventUpdate = await Event.findByIdAndUpdate(id, newEvent, {
+          const eventUpdate = await Event.findByIdAndUpdate(id,newEvent, {
             new: true,
-          })
-          //.populate('artist')
-          //.populate('assistants', 'username')
-          //.populate('organizer', 'username');
+          }).populate('artist')
+          .populate('assistants', 'username')
+          .populate('organizer', 'username');
           return res.status(200).json({mensaje:"Evento Actualizado", event:eventUpdate});
         } catch (error) {
           console.log(error)
