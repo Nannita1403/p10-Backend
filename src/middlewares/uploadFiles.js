dc71d237117e695812b951d02c7c6b2b527b0ccb
Natalia <nannimagi@gmail.com>
2025-10-14 16:07:55 +0200

    connectcloudinary

--- src/middlewares/uploadFiles.js
+++ src/middlewares/uploadFiles.js
@@ -1,9 +1,6 @@
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const connectCloudinary = require('../config/cloudinary.js');
-
-connectCloudinary();
 
 const eventoStorage = new CloudinaryStorage({
   cloudinary: cloudinary,
