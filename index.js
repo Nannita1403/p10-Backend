07cbbac0e1e5ea55fde36a88fdae8015966fdad2
Natalia <nannimagi@gmail.com>
2025-10-15 14:17:56 +0200

    origin index

--- index.js
+++ index.js
@@ -6,34 +6,14 @@ const { connectCloudinary } = require("./src/config/cloudinary");
 const mainRouter = require("./src/api/routes/main");
 
 const app = express();
-
-const allowedOrigins = ['https://p10-frontend.vercel.app', 'http://localhost:3000'];
-
+const PORT = 3000;
 
 connectDB();
 connectCloudinary();
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true); 
-    } else {
-      return callback(new Error('CORS no permitido para este origen'));
-    }
-  },
-  credentials: true, 
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-app.options('*', cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
-
-
+app.use(cors());
 app.use(express.json());    
+
 app.use('/api/v1', mainRouter);
 
 app.use("*", (req,res,next)=> {
@@ -46,8 +26,6 @@ app.use((err, req, res, next) => {
     return res.status(err.status || 500).json(err.message || 'Unexpected error');
   });
 
-//app.listen(PORT, ()=> {
-//    console.log('http://localhost:'+ PORT);
-//});
-
-module.exports = app;
+app.listen(PORT, ()=> {
+    console.log('http://localhost:'+ PORT);
+});
